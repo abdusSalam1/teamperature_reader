99dd9e1b7d6dd67b097705956f81ec06cf7960e9
abdussalam <abdus.salam@clickchain.com>
2019-04-12 10:37:03 +0500

    created method to get yearly temperature report by merging raw temp

--- src/domain/YearlyTemperatureReport.py
+++ src/domain/YearlyTemperatureReport.py
@@ -5,7 +5,16 @@ class YearlyTemperatureReport:
     def __init__(self):
         pass
 
-    def __init__(self, year, min_temperature, max_temperature):
-        self.year = year
-        self.min_temperature = min_temperature
-        self.max_temperature = max_temperature
+    def merge(self, raw_temperature):
+        raw_temperature_entries = raw_temperature.split(',')
+        self.merge_max_temperature(raw_temperature_entries[1])
+        self.merge_min_temperature(raw_temperature_entries[3])
+        self.year = raw_temperature_entries.split('-')[0]
+
+    def merge_max_temperature(self, max_temperature):
+        if max_temperature != "" and self.max_temperature > max_temperature:
+            self.max_temperature = max_temperature
+
+    def merge_min_temperature(self, min_temperature):
+        if min_temperature != "" and self.min_temperature > min_temperature:
+            self.min_temperature = min_temperature
--- src/expert/WeatherFileExpert.py
+++ src/expert/WeatherFileExpert.py
@@ -2,15 +2,19 @@ import glob
 import logging
 import os
 
+from domain.YearlyTemperatureReport import YearlyTemperatureReport
+
 
 class WeatherFileExert:
+
+    FILE_EXTENSION = "*.txt"
 
     def __init__(self):
         pass
 
     def read_yearly_weather(self, directory_path):
         try:
-            readable_files = glob.glob(os.path.join(directory_path, "*.txt"))
+            readable_files = glob.glob(os.path.join(directory_path, self.FILE_EXTENSION))
             for file in readable_files:
                 daily_temperatures = open(file, 'r').readlines()
                 self.get_yearly_temperature_report(daily_temperatures)
@@ -18,9 +22,11 @@ class WeatherFileExert:
             logging.error('Error while reading file')
 
     def get_yearly_temperature_report(self, daily_temperatures):
-        for temperature in daily_temperatures:
-            if self.is_valid_temperature(temperature):
-                pass
+        yearly_temperature_report = YearlyTemperatureReport()
+        for raw_temperature in daily_temperatures:
+            if self.is_valid_temperature(raw_temperature):
+                yearly_temperature_report.merge(raw_temperature)
+        return yearly_temperature_report
 
     def is_valid_temperature(self, data):
         return data.strip() and not data.startswith("<!--")
