7c300ebd5bfaf7a427d0201db095ce3c0af641de
abdussalam <abdus.salam@clickchain.com>
2019-04-12 10:01:49 +0500

    created yearly temperature report

---
+++ src/domain/YearlyTemperatureReport.py
@@ -0,0 +1,8 @@
+class YearlyTemperatureReport:
+    year = ""
+    min_temperature = max_temperature = 0.0
+
+    def __init__(self, year, min_temperature, max_temperature):
+        self.year = yaer
+        self.min_temperature = min_temperature
+        self.max_temperature = max_temperature
