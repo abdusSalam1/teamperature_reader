6a1ccf281a6b4fe4b513e5b8965aa5c9bebecbdb
abdussalam <abdus.salam@clickchain.com>
2019-04-12 10:22:50 +0500

    created domain to get yearly report from weather file expert

--- src/domain/YearlyTemperatureReport.py
+++ src/domain/YearlyTemperatureReport.py
@@ -2,7 +2,10 @@ class YearlyTemperatureReport:
     year = ""
     min_temperature = max_temperature = 0.0
 
+    def __init__(self):
+        pass
+
     def __init__(self, year, min_temperature, max_temperature):
-        self.year = yaer
+        self.year = year
         self.min_temperature = min_temperature
         self.max_temperature = max_temperature
--- src/expert/WeatherFileExpert.py
+++ src/expert/WeatherFileExpert.py
@@ -1,4 +1,5 @@
 import glob
+import logging
 import os
 
 
@@ -7,10 +8,19 @@ class WeatherFileExert:
     def __init__(self):
         pass
 
-    def read_weather(self, directory_path):
-        readable_files = glob.glob(os.path.join(directory_path, "*.txt"))
-        for file in readable_files:
-            raw_data = open(file, 'r').readlines()
-        for data in raw_data:
-            if data.strip() and not data.startswith("<!--"):
+    def read_yearly_weather(self, directory_path):
+        try:
+            readable_files = glob.glob(os.path.join(directory_path, "*.txt"))
+            for file in readable_files:
+                daily_temperatures = open(file, 'r').readlines()
+                self.get_yearly_temperature_report(daily_temperatures)
+        except:
+            logging.error('Error while reading file')
+
+    def get_yearly_temperature_report(self, daily_temperatures):
+        for temperature in daily_temperatures:
+            if self.is_valid_temperature(temperature):
                 pass
+
+    def is_valid_temperature(self, data):
+        return data.strip() and not data.startswith("<!--")
