9ecd05b226ed2759f143d6d0fc19c5fe564eab8d
abdussalam <abdus.salam@clickchain.com>
2019-04-15 22:48:41 +0500

    more

--- src/expert/WeatherReportExpert.py
+++ src/expert/WeatherReportExpert.py
@@ -9,4 +9,5 @@ class WeatherFileExpert:
         self.weatherFileExpert = WeatherFileExpert()
 
     def create_weather_report(self):
+        # Here we have got the yearly report with min and max temperature in a year
         yearly_weather_reports = self.weatherFileExpert.read_yearly_weather(self.DIRECTORY_PATH)
