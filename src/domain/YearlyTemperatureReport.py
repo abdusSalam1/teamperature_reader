676973b34f772956c2cc74c5e7d8db272f31529c
abdussalam <abdus.salam@clickchain.com>
2019-04-18 01:17:51 +0500

    fixing more errors

--- src/domain/YearlyTemperatureReport.py
+++ src/domain/YearlyTemperatureReport.py
@@ -7,9 +7,9 @@ class YearlyTemperatureReport:
 
     def merge(self, raw_temperature):
         raw_temperature_entries = raw_temperature.split(',')
-        self.merge_max_temperature(raw_temperature_entries[1])
-        self.merge_min_temperature(raw_temperature_entries[3])
-        self.year = raw_temperature_entries.split('-')[0]
+        self.merge_max_temperature(self.get_float_value(raw_temperature_entries[1]))
+        self.merge_min_temperature(self.get_float_value(raw_temperature_entries[3]))
+        self.year = raw_temperature_entries[0].split('-')[0]
 
     def merge_max_temperature(self, max_temperature):
         if max_temperature != "" and self.max_temperature > max_temperature:
@@ -18,3 +18,6 @@ class YearlyTemperatureReport:
     def merge_min_temperature(self, min_temperature):
         if min_temperature != "" and self.min_temperature > min_temperature:
             self.min_temperature = min_temperature
+
+    def get_float_value(self, value):
+        return float(value) if not value == '' else 0.0
